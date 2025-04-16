0f49092e755f3918ffec14366f3295b35860c0fa
simonlebeaud <lebeaud.simon@gmail.com>
2025-04-16 15:44:56 +0200

    Chapitre 2 terminé

--- jeu_du_plus_ou_du_moins/src/main.rs
+++ jeu_du_plus_ou_du_moins/src/main.rs
@@ -1,15 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Deviner le nombre !");
 
-    println!("Veuillez entrer un nombre");
+    let nombre_secret = rand::rng().random_range(1..101);
 
-    let mut supposition = String::new();
+    loop {
+        println!("Veuillez entrer un nombre");
 
-    io::stdin()
-        .read_line(&mut supposition)
-        .expect("Echech de la lecture de l'entrée utilisateur");
+        let mut supposition = String::new();
 
-    println!("Votre nombre : {}", supposition);
+        io::stdin()
+            .read_line(&mut supposition)
+            .expect("Echech de la lecture de l'entrée utilisateur");
+
+        let supposition: u32 = match supposition.trim().parse() {
+            Ok(nombre) => nombre,
+            Err(_) => continue,
+        };
+
+        println!("Votre nombre : {}", supposition);
+
+        match supposition.cmp(&nombre_secret) {
+            Ordering::Less => println!("C'est plus !"),
+            Ordering::Greater=> println!("C'est moins!"),
+            Ordering::Equal=> {
+                println!("Vous avez gagné !");
+                break;
+            }
+        }
+    }
+    
 }
