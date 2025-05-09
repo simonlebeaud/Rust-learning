98d35c3315972eeb92a5a550d9804d5c758caf1b
simonlebeaud <lebeaud.simon@gmail.com>
2025-05-09 20:17:47 +0200

    fin chapitre 12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,12 +8,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problème rencontré lors de l'interprétation des arguments : {}", err);
+        eprintln!("Problème rencontré lors de l'interprétation des arguments : {}", err);
+        
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Erreur applicative : {}", e);
+        eprintln!("Erreur applicative : {}", e);
+
         process::exit(1);
     }
 }
