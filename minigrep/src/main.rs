d454834a8e78b14885c54888364ef84fbfecd749
simonlebeaud <lebeaud.simon@gmail.com>
2025-05-09 19:33:46 +0200

    chapitre 12.3

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,23 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
+
     let args: Vec<String> = env::args().collect();
-    
-    let recherche = &args[1];
-    let nom_fichier = &args[2];
-    
-    println!("On recherche : {}", recherche);
-    println!("Dans le fichier : {}", nom_fichier);
 
-    let contenu = fs::read_to_string(nom_fichier)
-        .expect("Quelque chose s'est mal passé lors de la lecture du fichier !");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problème rencontré lors de l'interprétation des arguments : {}", err);
+        process::exit(1);
+    });
 
-    println!("Dans le texte :\n{}", contenu);
+    println!("On recherche : {}", config.recherche);
+    println!("Dans le fichier : {}", config.nom_fichier);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Erreur applicative : {}", e);
+
+        process::exit(1);
+    }
 }
