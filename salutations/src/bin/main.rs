7802420140db0848a12b46d7a094cd933fae18bb
simonlebeaud <lebeaud.simon@gmail.com>
2025-05-21 15:24:57 +0200

    chapitre 20.2

--- salutations/src/bin/main.rs
+++ salutations/src/bin/main.rs
@@ -1,19 +1,24 @@
 use std::fs;
+use std::thread;
+use std::time::Duration;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use salutations::GroupeTaches;
 
 
 fn main() {
     let ecouteur = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let groupe = GroupeTaches::new(4);
 
     for flux in ecouteur.incoming() {
         let flux = flux.unwrap();
 
-        gestion_connexion(flux);
+        groupe.executer(|| {
+            gestion_connexion(flux);
+        });
     }
 }
-
 
 
 fn gestion_connexion(mut flux: TcpStream) {
@@ -22,8 +27,12 @@ fn gestion_connexion(mut flux: TcpStream) {
     flux.read(&mut tampon).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let pause = b"GET /pause HTTP/1.1\r\n";
 
     let (ligne_statut, nom_fichier) = if tampon.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if tampon.starts_with(pause) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
